fa8617ef1008e84c424ccb9d80db7f7ca91d2676
alastair-j-hewitt <alastairjohnhewitt@gmail.com>
2024-09-30 11:24:56 -0300

    Fix Webpack configuration and remove invalid properties

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.merge({
+  node: {
+    __dirname: true,
+    __filename: true,
+    global: true
+  }
+})
+
 module.exports = environment
